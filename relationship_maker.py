61872aae77aaccf1782a07f858b86236beedb8b2
LunaLii <48501012+LunaLii@users.noreply.github.com>
2019-03-22 14:35:53 +1300

    Delete relationship_maker.py

--- relationship_maker.py
+++
@@ -1,6 +0,0 @@
-class RelationshipMaker:
-    def __init__(self, new_relationship):
-        self.relationship = new_relationship
-
-    def __str__(self):
-        return f"       my{self.relationship} = []"
