2a4d82cff423afe655bbf320afe253dfb5af287f
Rajan Gurung <rajangrg1@hotmail.com>
2019-03-22 14:20:03 +1300

    pie chart creating

--- chart_maker.py
+++ chart_maker.py
@@ -9,3 +9,13 @@ class ChartMaker:
         plt.bar(range(len(sizes)), sizes, tick_label=name_list)
         plt.title("The numbers of class, attributes and methods")
         plt.show()
+        
+    def create_pie_chart(self, all_num):
+        plt.figure(figsize=(5, 5))  # blank pie chart will 5 inch by 5inch
+        labels = ["ClassNum", "AttributeNum", "MethodNum"]  # creating a labels for class
+        values = all_num  # adding values to the labels
+        explode = [0.05, 0, 0]  # this is used to highlight the biggest percent
+        # creating a function to create a pie chart
+        plt.pie(values, labels=labels, autopct="%.1f%%", explode=explode)  # autopct is a 'paramater'
+
+        plt.show()  # to show the blank diag
