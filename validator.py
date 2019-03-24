db86bde06036a8d2b408a5e71b3e8b349f87925c
cey0030 <48426058+cey0030@users.noreply.github.com>
2019-03-24 15:36:59 +1300

    Update validator.py

--- validator.py
+++ validator.py
@@ -1,6 +1,6 @@
 class Validator:
     @staticmethod
-    def validate_class_name(class_name):
+    def validate_class_name(name):
         """
         >>> Validator.validate_class_name("ClassBuilder")
         True
@@ -8,10 +8,35 @@ class Validator:
         >>> Validator.validate_class_name("classBuilder")
         False
         """
-        if class_name[0].isupper():
+        if name[0].isupper():
             return True
         else:
-            return  False
+            return False
+
+    # By Clement
+    @staticmethod
+    def validate_attribute_name(name):
+        """
+        >>> Validator.validate_attribute_name("Name")
+        False
+        >>> Validator.validate_attribute_name("break")
+        False
+        >>> Validator.validate_attribute_name("attributedfjkslsdjfkdslfj;dkslfjskdlfj;dskl;fjkslfjkal;jfdkla;jfksljfkl;ajfdk;ljafkld;jfkjfakld;jfkd;fa")
+        False
+        >>> Validator.validate_attribute_name("a")
+        False
+        >>> Validator.validate_attribute_name(1)
+        False
+        >>> Validator.validate_attribute_name("attribute")
+        True
+        """
+        reserved = ['and', 'assert', 'break', 'class', 'continue', 'def', 'del', 'elif', 'else', 'except', 'exec',
+                    'finally', 'for', 'from', 'global', 'if', 'import', 'in', 'is', 'lambda', 'not', 'or', 'pass',
+                    'print', 'raise', 'return', 'try', 'while']
+        if not isinstance(name, str) or name[0].isupper() or name in reserved or not 1 < len(name) < 31:
+            return False
+        else:
+            return True
 
 
 if __name__ == "__main__":
@@ -19,13 +44,4 @@ if __name__ == "__main__":
     doctest.testmod(verbose=True)
 
 
-    # @staticmethod
-    # def validate_attribute_name(a_name):
-    #     """
-    #     >>> Validator.validate_class_name("name")
-    #     True
-    #     """
-    #     if a_name.islower():
-    #         return True
-    #     else:
-    #         return False
+
