c347a748eef5638255f2594cfdca85229a23b8db
LunaLii <48501012+LunaLii@users.noreply.github.com>
2019-03-24 15:57:52 +1300

    validate class name

--- validator.py
+++ validator.py
@@ -1,17 +1,28 @@
+import re
+
+
 class Validator:
     @staticmethod
     def validate_class_name(class_name):
         """
         >>> Validator.validate_class_name("ClassBuilder")
         True
-
+        >>> Validator.validate_class_name("ClassName123")
+        True
         >>> Validator.validate_class_name("classBuilder")
         False
+        >>> Validator.validate_class_name("C!#Name")
+        False
+        >>> Validator.validate_class_name("ClassName>")
+        False
+        >>> Validator.validate_class_name("1ClassName>")
+        False
         """
-        if class_name[0].isupper():
+        regex = re.compile('[@_!#$%^&*()<>?/|}{~:]')
+        if class_name[0].isupper() and regex.search(class_name) is None:
             return True
         else:
-            return  False
+            return False
 
 
 if __name__ == "__main__":
@@ -19,13 +30,3 @@ if __name__ == "__main__":
     doctest.testmod(verbose=True)
 
 
-    # @staticmethod
-    # def validate_attribute_name(a_name):
-    #     """
-    #     >>> Validator.validate_class_name("name")
-    #     True
-    #     """
-    #     if a_name.islower():
-    #         return True
-    #     else:
-    #         return False
